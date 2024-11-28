0b334217448b01ed1c915dbdaf3d43bc09ed1db7
MouhamedAliSaidi <medalisaidi009@gmail.com>
2024-11-28 17:20:49 +0100

    added test99

---
+++ test99/test.py
@@ -0,0 +1,6 @@
+# 1. TASK: print "Hello World"
+print("Hello World")
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Mouhamed Ali"
+print( "Hello",name )	# with a comma
+print( "Hello " +name )	# with a +
